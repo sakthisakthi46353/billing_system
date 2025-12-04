0271711e551bac9bd51446361c31be9484f148d0
sakthisakthi46353 <sakthisakthi46353@gmail.com>
2025-12-04 14:52:22 +0530

    EOD: Milestone 2 basic Customer & Product CRUD

--- core/models.py
+++ core/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 
 class Customer(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField(blank=True, null=True)
-    phone = models.CharField(max_length=20)
-    address = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
+
+# Milestone 3 new models
+class Invoice(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, default="UNPAID")
+
+class InvoiceItem(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Payment(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
