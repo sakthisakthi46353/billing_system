f2f711e09fc9754aa7f28507aa34e999d1e413d7
sakthisakthi46353 <sakthisakthi46353@gmail.com>
2025-12-19 15:16:36 +0530

    Initial commit - Django Billing System

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,6 @@ app_name = "products"
 urlpatterns = [
     path("", views.product_list, name="product_list"),
     path("add/", views.product_add, name="product_add"),
-    path("<int:pk>/", views.product_view, name="product_view"),  # 👈 ADD
     path("<int:pk>/edit/", views.product_edit, name="product_edit"),
     path("<int:pk>/delete/", views.product_delete, name="product_delete"),
 ]
